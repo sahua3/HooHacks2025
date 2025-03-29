09982c4dd2a6f02e3024f5881d4f1e624ea89f58
sahua3 <sahua3@vcu.edu>
2025-03-29 15:44:06 -0400

    quiz.js ready for backend implementation

--- exquizit/src/index.js
+++ exquizit/src/index.js
@@ -4,13 +4,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import Gemini from './gemini';
 import HomePage from './Homepage';
+import Quiz from './Quiz';
 
 export default function Index() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+<<<<<<< HEAD
         <Route path="/gemini" element={<Gemini />} />
+=======
+        <Route path="quiz" element={<Quiz />} />
+>>>>>>> a06c207 (quiz.js ready for backend implementation)
       </Routes>
     </BrowserRouter>
   );
