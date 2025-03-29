a97c21988b95faab7bd0afe26a7bc4fb041374df
sahua3 <sahua3@vcu.edu>
2025-03-29 15:05:15 -0400

    created homepage

--- exquizit/src/index.js
+++ exquizit/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import Gemini from './gemini';
-import reportWebVitals from './reportWebVitals';
+import HomePage from './Homepage';
+
+export default function Index() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/gemini" element={<Gemini />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Gemini />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Index />);
