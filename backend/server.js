743121c78e2b86778c9f9190fddf2f132563003a
DEVIN VEASNA <veasnadt@vcu.edu>
2025-03-29 15:08:40 -0400

    client test for backend done

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import quizRoutes from "./routes/quizRoutes.js";
-<<<<<<< HEAD
 import questionRoutes from "./routes/questionRoutes.js";
-=======
->>>>>>> 13ae2e8 (mongoDB implemented)
+import questionRoutes from "./routes/questionRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -17,19 +15,12 @@ app.use(express.json());
 
 app.use("/api/quiz", quizRoutes);
 
-<<<<<<< HEAD
 app.use("/api/questions", questionRoutes);
 
 app.get("/", (req, res) => res.send("exQuizit backend is live!"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
-=======
-app.get("/", (req, res) => res.send("🎓 exQuizit backend is live!"));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
->>>>>>> 13ae2e8 (mongoDB implemented)
 
 app.get("/test-db", async (req, res) => {
     try {
