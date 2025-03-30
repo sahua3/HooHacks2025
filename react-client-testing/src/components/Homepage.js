3d0015fad78e3f22ce351f310fb4cf33fb3f4c8f
DEVIN VEASNA <veasnadt@vcu.edu>
2025-03-30 04:17:09 -0400

    homepage css edits

--- react-client-testing/src/components/Homepage.js
+++ react-client-testing/src/components/Homepage.js
@@ -7,11 +7,12 @@ function HomePage() {
   return (
     <div className="HomePage">
       <div className="App-header">
+        <h1>exQuizit</h1>
         <img src={bunny} width="400" height="200" alt="bunny"/> 
         
         <div className="body">
-          <Button btnTxt={'PLAY'} route="/start"/>
-          <Button btnTxt={'HOW TO PLAY'}/>
+          <Button className="btn" btnTxt={'PLAY'} route="/start"/>
+          <Button className="btn" btnTxt={'HOW TO PLAY'}/>
             <div className="footer">
                 <p>Made with ❤️ by the ExQuizit Team</p>
                 <p>Version 1.0.0</p>
